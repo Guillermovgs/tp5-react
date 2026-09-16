9842e42687cc041acf058f931e88a74b85d8f2b5
Guillermovgs <guillermosanchez0501@gmail.com>
2026-09-16 20:02:54 -0300

    al fin cambios 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
->>>>>>> master
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< HEAD
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
   <React.StrictMode>
     <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
 );
->>>>>>> master
